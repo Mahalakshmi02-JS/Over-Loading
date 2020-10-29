fd8d3d4602e9c0122efdf28fbfd0b3b3cfe4049a
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-30 00:35:55 +0530

    Add files via upload

---
+++ OlympicTester.java
@@ -0,0 +1,13 @@
+public class OlympicTester{
+
+public static void main(String[] play)
+{
+    System.out.println("JVM invoked main");		
+        
+		Olympic.games("Vishwanathanan Anand","INDIA");
+		Olympic.games("INDIA",10);
+		Olympic.games(23,"Swimming",42.195);
+
+			System.out.println("EXIT :: main");	
+}
+}
