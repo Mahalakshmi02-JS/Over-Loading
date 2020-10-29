ad27964fc606bbdbc37619c26aacda5f11fbe7a2
Mahalakshmi02-JS <72906335+Mahalakshmi02-JS@users.noreply.github.com>
2020-10-30 00:39:27 +0530

    PlaceSearcher(Travelling)

---
+++ Travelling.java
@@ -0,0 +1,83 @@
+public class Travelling{
+	public static String search(int distance)
+	{
+		System.out.println("Invoked distance :" +distance);
+		if(distance==182)
+		{
+			System.out.println("Banglore");
+			return "Banglore";
+		}
+		if(distance==883)
+		{
+			System.out.println("Maharashthra");
+			return "Maharashthra";
+		}
+		if(distance==103)
+		{
+			System.out.println("Madikeri");
+			return "Madikeri";
+		}
+		if(distance==190)
+		{
+			System.out.print("Manglore");
+			return "Manglore";
+		}
+		if(distance==2349)
+		{
+			System.out.println("Delhi");
+			return "Delhi";
+		}
+		if(distance==60)
+		{
+			System.out.println("Chikmagalore");
+			return "Chikmagalore";
+		}
+		if(distance==152)
+		{
+			System.out.println("Shivmogha");
+			return "Shivmogha";
+		}
+		if(distance==33)
+		{
+			System.out.println("Halebidu");
+			return "Halebidu";
+		}
+		if(distance==179)
+		{
+			System.out.println("Chitradurga");
+			return "Chitradurga";
+		}
+		if(distance==503)
+		{
+			System.out.println("Tamilnadu");
+			return "Tamilnadu";
+		}
+		if(distance==330)
+		{
+			System.out.println("Kerala");
+			return "Kerala";
+		}
+		if(distance==1582)
+		{
+			System.out.println("Gujarath");
+			return "Gujarath";
+		}
+		if(distance==2025)
+		{
+			System.out.println("kolkatta");
+			return "kolkatta";
+		}
+		if(distance==39)
+		{
+			System.out.println("Sakaleshpur");
+			return "Sakaleshpur";
+		}
+		if(distance==246)
+		{
+			System.out.println("kolar");
+			return "kolar";
+		}
+		System.out.println("Place not in Earth");
+		return "place not in Earth";
+	}
+}
